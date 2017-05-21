9803a578d4308032fb63ab7a9f841f7fd18553c1
Steveo00 <srwillis@ozemail.com.au>
2017-05-21 18:57:46 +1000

    easy 1 and easy 2 solutions

---
+++ easy_2_2.rb
@@ -0,0 +1,15 @@
+# "The munsters are creepy in a good way."
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+# "the munsters are creepy in a good way."
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+
+
+munsters_description = "The Munsters are creepy in a good way."
+
+puts munsters_description.capitalize
+
+puts munsters_description.swapcase
+
+puts munsters_description.downcase
+
+puts munsters_description.upcase
